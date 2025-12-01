9791102b62bc8892e2c13dd8f6b32011fdb18195
josemag <josemag@uci.cu>
2025-12-01 16:49:25 -0500

    Cambios en importe de iconos

--- components/Sidebar.jsx
+++ components/Sidebar.jsx
@@ -11,6 +11,8 @@ import {
   ShoppingCart,
   UserCircle,
   FileSpreadsheet,
+  PanelLeftOpen,
+  PanelLeftClose,
 } from "lucide-react";
 import { cn } from "@/lib/utils";
 import { Button } from "@/components/ui/button";
@@ -44,26 +46,13 @@ export default function SidebarHandler() {
         <div className="flex h-full md:w-64 flex-col bg-card border-r overflow-contain">
           <div className="flex h-16 items-center px-6 border-b">
             <button
-              className="flex"
+              className="flex text-blue-800"
               onClick={() => setSidebarOpen(!sidebarOpen)}
             >
-              <h1 className="text-xl font-bold text-blue-800 mx-1">
+              <h1 className="text-xl font-bold">
                 <span className="text-red-800">E</span>líneas
               </h1>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth={1.5}
-                stroke="currentColor"
-                className="size-6"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M8.25 9V5.25A2.25 2.25 0 0 1 10.5 3h6a2.25 2.25 0 0 1 2.25 2.25v13.5A2.25 2.25 0 0 1 16.5 21h-6a2.25 2.25 0 0 1-2.25-2.25V15m-3 0-3-3m0 0 3-3m-3 3H15"
-                />
-              </svg>
+              <PanelLeftClose className="h-7 ml-1" />
             </button>
           </div>
           <nav className="flex-1 space-y-1 px-3 py-4">
@@ -109,25 +98,15 @@ export default function SidebarHandler() {
       open={sidebarOpen}
       onSetOpen={setSidebarOpen}
     >
-      <div className="flex h-16 items-center px-6 bg-card border-b">
-        <button className="flex" onClick={() => setSidebarOpen(!sidebarOpen)}>
-          <h1 className="text-xl font-bold text-blue-800 mx-1">
+      <div className="flex fixed w-full h-16 items-center px-6 bg-card border-b">
+        <button
+          className="flex text-blue-800"
+          onClick={() => setSidebarOpen(!sidebarOpen)}
+        >
+          <h1 className="text-xl font-bold">
             <span className="text-red-800">E</span>líneas
           </h1>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            strokeWidth={1.5}
-            stroke="currentColor"
-            className="size-6"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M8.25 9V5.25A2.25 2.25 0 0 1 10.5 3h6a2.25 2.25 0 0 1 2.25 2.25v13.5A2.25 2.25 0 0 1 16.5 21h-6a2.25 2.25 0 0 1-2.25-2.25V15M12 9l3 3m0 0-3 3m3-3H2.25"
-            />
-          </svg>
+          <PanelLeftOpen className="h-7 ml-1" />
         </button>
       </div>
     </Sidebar>
