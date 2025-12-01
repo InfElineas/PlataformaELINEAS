aa5a2248ae15685b3d4708e57f6e4f06e3b94a48
josemag <josemag@uci.cu>
2025-12-01 10:34:41 -0500

    Botón de despliegue estilizado.

--- components/Sidebar.jsx
+++ components/Sidebar.jsx
@@ -43,10 +43,27 @@ export default function SidebarHandler() {
       sidebar={
         <div className="flex h-full md:w-64 flex-col bg-card border-r overflow-contain">
           <div className="flex h-16 items-center px-6 border-b">
-            <button onClick={() => setSidebarOpen(!sidebarOpen)}>
-              <h1 className="text-xl font-bold text-primary text-blue-800">
+            <button
+              className="flex"
+              onClick={() => setSidebarOpen(!sidebarOpen)}
+            >
+              <h1 className="text-xl font-bold text-primary text-blue-800 mx-1">
                 <span className="text-red-800">E</span>líneas
               </h1>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="size-6"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M8.25 9V5.25A2.25 2.25 0 0 1 10.5 3h6a2.25 2.25 0 0 1 2.25 2.25v13.5A2.25 2.25 0 0 1 16.5 21h-6a2.25 2.25 0 0 1-2.25-2.25V15m-3 0-3-3m0 0 3-3m-3 3H15"
+                />
+              </svg>
             </button>
           </div>
           <nav className="flex-1 space-y-1 px-3 py-4">
@@ -92,9 +109,27 @@ export default function SidebarHandler() {
       open={sidebarOpen}
       onSetOpen={setSidebarOpen}
     >
-      <button onClick={() => setSidebarOpen(!sidebarOpen)}>
-        Abrir barra lateral
-      </button>
+      <div className="flex h-16 items-center px-6 bg-card border-b">
+        <button className="flex" onClick={() => setSidebarOpen(!sidebarOpen)}>
+          <h1 className="text-xl font-bold text-primary text-blue-800 mx-1">
+            <span className="text-red-800">E</span>líneas
+          </h1>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="size-6"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M8.25 9V5.25A2.25 2.25 0 0 1 10.5 3h6a2.25 2.25 0 0 1 2.25 2.25v13.5A2.25 2.25 0 0 1 16.5 21h-6a2.25 2.25 0 0 1-2.25-2.25V15M12 9l3 3m0 0-3 3m3-3H2.25"
+            />
+          </svg>
+        </button>
+      </div>
     </Sidebar>
   );
 }
